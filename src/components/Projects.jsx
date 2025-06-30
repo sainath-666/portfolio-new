f3bef64452b3f37e328f6c461fe587db71b10bdb
Sainathreddy <sai65265@gmail.com>
2025-07-01 03:37:33 +0530

    small change in project and later i will update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -44,7 +44,7 @@ const Projects = () => {
                 href={project.github}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-blue-500 hover:bg-blue-600 px-4 py-1 rounded-md text-sm"
+            className="bg-blue-500 hover:bg-blue-600 hover:shadow-blue-500/40 hover:shadow-lg text-white px-6 py-2 rounded-md font-medium transition duration-300"
               >
                 GitHub
               </a>
@@ -52,7 +52,7 @@ const Projects = () => {
                 href={project.live}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="border border-blue-400 hover:bg-blue-500 hover:text-white px-4 py-1 rounded-md text-sm"
+            className="border border-blue-400 text-blue-400 hover:bg-blue-500 hover:text-white hover:shadow-blue-400/40 hover:shadow-lg px-6 py-2 rounded-md font-medium transition duration-300"
               >
                 Live Demo
               </a>
